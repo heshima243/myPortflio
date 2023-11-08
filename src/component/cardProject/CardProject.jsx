578eba5a1fe61ad1e02827234728125a295b61e0
heshima243 <heshimajulien.official@gmail.com>
2023-11-08 11:36:21 +0100

    update

--- src/component/cardProject/CardProject.jsx
+++ src/component/cardProject/CardProject.jsx
@@ -15,7 +15,7 @@ const images = [
   {
     imgPath:
       "https://i.pinimg.com/736x/3f/4b/97/3f4b977239687f87feabda7add3ca0f5.jpg",
-    title: "WeatherApp usinf API",
+    title: "WeatherApp using API",
     description:'https://heshima-meteo.netlify.app/'
   },
   {
@@ -23,6 +23,12 @@ const images = [
       "https://i.pinimg.com/736x/a3/51/cb/a351cb9228eb61b6d6567fdb389d0973.jpg",
     title: "showcase website using React and Sanity",
     description:'https://ilc-center.vercel.app/'
+  },
+  {
+    imgPath:
+      'https://i.pinimg.com/736x/33/d2/d0/33d2d0e152c65a19081b3928b442dc7e.jpg',
+    title: "Fake-Store connected to API fake-store",
+    description:'https://heshima-store-online.vercel.app'
   },
  
 ];
@@ -38,14 +44,12 @@ export default function CardProject() {
       <Box
         sx={{
           display: "flex",
-          justifyContent: isSmallScreen ? "center" : "start", // Centrer sur les petits écrans
+          justifyContent: isSmallScreen ? "center" : "start",
           alignItems: "center",
           gap:3,
           flexWrap:'wrap'
         }}
       >
-
-
         {images.map((image, index) => (
           <Card
             key={index}
